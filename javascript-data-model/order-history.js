696d7d33487f93fdc1ed8b3080fee746a1e124c1
Minji Cho <mturtle051@gmail.com>
2022-03-22 20:02:32 +0000

    Edited2 javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,13 +1,13 @@
 var orderHistory = [
   {
+    orderPlaced: 'Aug 4, 2020',
+    total: 34.00,
+    shipTo: 'JS Masher',
+    deliverd: 'Aug 8, 2020',
+    orderDetails: null,
+    invoice: null,
     items: [
       {
-        orderPlaced: 'Aug 4, 2020',
-        total: 34.00,
-        shipTo: 'JS Masher',
-        deliverd: 'Aug 8, 2020',
-        orderDetails: null,
-        invoice: null,
         delevered: 'Aug 8, 2020',
         numberOfOrderedItem: 1,
         bookTitle: 'JavaScript for impatient programmers',
@@ -22,14 +22,14 @@ var orderHistory = [
     ]
   },
   {
+    orderPlaced: 'July 19, 2020',
+    total: 44.53,
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    orderDetails: null,
+    invoice: null,
     items: [
       {
-        orderPlaced: 'July 19, 2020',
-        total: 44.53,
-        shipTo: 'JS Masher',
-        orderNumber: '113-9984268-1280257',
-        orderDetails: null,
-        invoice: null,
         delevered: 'Jul 20, 2020',
         numberOfOrderedItem: 1,
         bookTitle: 'The Timeless Way of Building',
@@ -44,14 +44,14 @@ var orderHistory = [
     ]
   },
   {
+    orderPlaced: 'July 4, 2020',
+    total: 17.22,
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    orderDetails: null,
+    invoice: null,
     items: [
       {
-        orderPlaced: 'July 4, 2020',
-        total: 17.22,
-        shipTo: 'JS Masher',
-        orderNumber: '114-2875557-9059409',
-        orderDetails: null,
-        invoice: null,
         delevered: 'Jul 7, 2020',
         numberOfOrderedItem: 1,
         bookTitle: null,
@@ -66,14 +66,14 @@ var orderHistory = [
     ]
   },
   {
+    orderPlaced: 'July 3, 2020',
+    total: 138.93,
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    orderDetails: null,
+    invoice: null,
     items: [
       {
-        orderPlaced: 'July 3, 2020',
-        total: 138.93,
-        shipTo: 'JS Masher',
-        orderNumber: '113-2883177-2648248',
-        orderDetails: null,
-        invoice: null,
         delevered: 'Jul 5, 2020',
         numberOfOrderedItem: 2,
         bookTitle: null,
