45c4f00a06a5a2b744cf1147dfd3b2552a9685dc
Minji Cho <mturtle051@gmail.com>
2022-03-22 19:24:46 +0000

    completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,90 @@
+var orderHistory = [
+  {
+    items: [
+      {
+        orderPlaced: 'Aug 4, 2020',
+        total: '$34.00',
+        shipTo: 'JS Masher',
+        deliverd: 'Aug 8, 2020',
+        orderDetails: null,
+        invoice: null,
+        delevered: 'Aug 8, 2020',
+        numberOfOrderedItem: 1,
+        bookTitle: 'JavaScript for impatient programmers',
+        itemNames: null,
+        author: 'Rauschmayer, Dr. Axel',
+        returnWindow: 'Return window closed on Sep 7, 2020',
+        itemPrice: ['$31.55'],
+        buyItAgain: null,
+        viewYourItem: null,
+        writeAProductReview: false
+      }
+    ]
+  },
+  {
+    items: [
+      {
+        orderPlaced: 'July 19, 2020',
+        total: '$44.53',
+        shipTo: 'JS Masher',
+        orderNumber: '113-9984268-1280257',
+        orderDetails: null,
+        invoice: null,
+        delevered: 'Jul 20, 2020',
+        numberOfOrderedItem: 1,
+        bookTitle: 'The Timeless Way of Building',
+        itemNames: null,
+        author: 'Alexander, Christopher',
+        returnWindow: 'Return window closed on Aug 19, 2020',
+        itemPrice: ['$41.33'],
+        buyItAgain: null,
+        viewYourItem: null,
+        writeAProductReview: false
+      }
+    ]
+  },
+  {
+    items: [
+      {
+        orderPlaced: 'July 4, 2020',
+        total: '$17.22',
+        shipTo: 'JS Masher',
+        orderNumber: '114-2875557-9059409',
+        orderDetails: null,
+        invoice: null,
+        delevered: 'Jul 7, 2020',
+        numberOfOrderedItem: 1,
+        bookTitle: null,
+        itemNames: ['Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter'],
+        author: null,
+        returnWindow: 'Return window closed on Aug 5, 2020',
+        itemPrice: ['$15.98'],
+        buyItAgain: null,
+        viewYourItem: null,
+        writeAProductReview: false
+      }
+    ]
+  },
+  {
+    items: [
+      {
+        orderPlaced: 'July 3, 2020',
+        total: '$138.93',
+        shipTo: 'JS Masher',
+        orderNumber: '113-2883177-2648248',
+        orderDetails: null,
+        invoice: null,
+        delevered: 'Jul 5, 2020',
+        numberOfOrderedItem: 2,
+        bookTitle: null,
+        itemNames: ['Gamecube Controller - Super Samsh Bros. Edition (Nintendo Switch', 'The Art of Sql'],
+        author: null,
+        returnWindow: 'Return window closed on Aug 4, 2020',
+        itemPrice: ['$94.95', '$33.99'],
+        buyItAgain: null,
+        viewYourItem: null,
+        writeAProductReview: false
+      }
+    ]
+  }
+];
