f84a4f16dea68784e68afcd00bdf194696a51603
Minji Cho <mturtle051@gmail.com>
2022-03-29 22:18:40 +0000

    Edited javascript-light-bulb

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,15 +1,15 @@
-var clicks = 0;
+var isLightOn = true;
 
 var buttonClick = document.querySelector('button');
 
 buttonClick.addEventListener('click', changeColor);
 
 function changeColor() {
-  clicks++;
-  if (clicks % 2 === 0) {
+  isLightOn = !isLightOn;
+  if (isLightOn === true) {
     document.querySelector('button').className = 'style-button-on';
     document.querySelector('body').className = 'color-on-background';
-  } else if (clicks % 2 === 1) {
+  } else {
     document.querySelector('button').className = 'style-button-off';
     document.querySelector('body').className = 'color-off-background';
   }
