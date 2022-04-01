bd687fd72404bef34954f98adb192a4d414ad2a7
Minji Cho <mturtle051@gmail.com>
2022-04-01 04:26:22 +0000

    Completed javascript-typing-tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,31 +1,30 @@
-var letters = document.querySelectorAll('span');
-var cursor = document.querySelector('.cursor');
 
+var $letters = document.querySelectorAll('span');
 document.addEventListener('keydown', typeStart);
-// itterate over string: for: of loop w/ counter variable
-// string[counter] = underlined
-// => if: string[counter] === event.key
-// change the color to correct, ++counter, remove underline @ counter-1
-// else: change color to red.
-console.log(letters[0].textContent);
+
+var correctLetterCount = 0; // for keeping track of the current index of the current letter.
 function typeStart(event) {
-  if (event.key === letters[0].textContent) {
-    letters[0].className = '.color-correct';
+  var $currentLetter = $letters[correctLetterCount];
+  if (event.key === $letters[correctLetterCount].textContent) {
+    $currentLetter.classList.add('color-correct');
+    $currentLetter.classList.remove('cursor');
+    $currentLetter.classList.remove('color-wrong', 'cursor-wrong');
+    correctLetterCount++;
+    $letters[correctLetterCount].classList.add('cursor');
+  } else {
+    $currentLetter.classList.add('cursor-wrong');
+    $currentLetter.classList.add('color-wrong');
+
   }
-  console.log(event.key);
 }
 
-// - query all span elements from HTML, store in a variable 'letters'
-// - query element by calling class name '.cursor', and store in a variable 'cursor'
-
-//   - create addEventListener for when any key is pressed, (keydown event) and create a   callback function.
-
-// - callback function named 'typeStart(event)'
-//   - in the function's code block:
-
-//     - iterate over letters by each index, and compare with the original string.
-//     - if first index of letters is correct to first letter of string,
-//   apply css class (.color - correct) to letter, and move the cursor
-//   to the next letter.
-//     - if wrong key  is pressed(key is not equal to string),
-//   apply css class (.color - wrong) to the letter, and cursor stays.
+// - new variable to keep track of the current index of the current letter
+//   (the user needs to type)
+// - compare key property of the event object to the textContent of the span for
+//   the current letter.
+// - if the letter was typed correctly, apply class for correct word (.color-correct),
+//   and move the cursor to the next letter, and increment your variable to keep track
+//   of the current index.
+//      = since the next letter is now the letter the user should type.
+// - if letter was typed incorrectly, apply class for wrong word,
+//   don't increment correctLetterCount.
