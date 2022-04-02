3f179b7c8fb034bc3b0431468caaf76a555b4881
Minji Cho <mturtle051@gmail.com>
2022-04-02 04:31:10 +0000

    Completed string-manipulation-2

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -7,10 +7,21 @@
 
 // output: string with characters at firstIndex and secondIndex swapped
 
-// - extract the firstIndex from string and store in a variable 'firstIdx'
-// - extract the secondIndex from string and store in a variable 'secondIdx'
-// - substring the first and second index
+// - split string into each character, and store them into a variable 'splitedStr'
+// - after split into each character, splitedStr is an array with separated characters.
+// - get the firstIndex of the splitedStr and store into a variable 'firstIdx'
+// - set the firstIndex of splitedStr to the secondIndex of the splitedStr.
+// - then set the secondIndex of splitedStr to firstIdx, so that the stored firstIndex
+//   of the character will be replaced with the secondIndex of the splitedStr.
+// - convert the splitedStr into string, joining each character into a single string,
+//   store the result into a vairable 'joinedStr'
+// - return the return value of 'joinedStr'
 
-// split string
-// split -> in indexes.
-//
+function swapChars(firstIndex, secondIndex, string) {
+  var splitedStr = string.split('');
+  var firstIdx = splitedStr[firstIndex];
+  splitedStr[firstIndex] = splitedStr[secondIndex];
+  splitedStr[secondIndex] = firstIdx;
+  var joinedStr = splitedStr.join('');
+  return joinedStr;
+}
