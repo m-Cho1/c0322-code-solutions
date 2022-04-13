03483d585531b2207f754d057b0aa0c1d3979fb9
Minji Cho <mturtle051@gmail.com>
2022-04-13 18:20:28 +0000

    Edited javascript-timers

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -2,21 +2,14 @@ var $countDown = document.querySelector('.countdown-display');
 
 var counter = 4;
 
-setInterval(countDown, 1000);
+var interval = setInterval(countDown, 1000);
 
 function countDown() {
   counter--;
-  if (counter === 3) {
-    $countDown.textContent = '3';
-  }
-  if (counter === 2) {
-    $countDown.textContent = '2';
-  }
-  if (counter === 1) {
-    $countDown.textContent = '1';
-  }
   if (counter === 0) {
     $countDown.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(interval);
+  } else {
+    $countDown.textContent = counter;
   }
-  clearInterval();
 }
