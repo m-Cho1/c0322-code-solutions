f917e57e0007c537eb81a9a999574e866780a002
Minji Cho <mturtle051@gmail.com>
2022-04-14 14:59:14 +0000

    progress2 javascript-carousel

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -3,21 +3,14 @@ var $arrowRight = document.querySelector('.arrow-right');
 var $image = document.querySelector('img');
 var $dotsContainer = document.querySelector('.buttons');
 
+var currentImageIndex = 0;
 var imageArr = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
 
 var $allDots = document.querySelectorAll('.img-dot');
 
-// var $dot1 = document.querySelector('#0');
-// var $dot2 = document.querySelector('#1');
-// var $dot3 = document.querySelector('#2');
-// var $dot4 = document.querySelector('#3');
-// var $dot5 = document.querySelector('#4');
-
 $arrowLeft.addEventListener('click', previousImgFunc);
 $arrowRight.addEventListener('click', nextImgFunc);
 $dotsContainer.addEventListener('click', indicatorFunc);
-
-var currentImageIndex = 0;
 
 function nextImgFunc() {
   currentImageIndex++;
@@ -47,11 +40,28 @@ function indicatorFunc(event) {
 
 function changeDotColor() {
   for (var i = 0; i < $allDots.length; i++) {
-    console.log('currentImageIndex: ', currentImageIndex, 'i: ', i);
+    // console.log('currentImageIndex: ', currentImageIndex, 'i: ', i);
     $allDots[i].className = 'far fa-circle';
     if (parseInt($allDots[i].getAttribute('id')) === currentImageIndex) {
-      console.log('if statement working');
+      // console.log('if statement working');
       $allDots[i].className = 'fas fa-circle';
     }
   }
 }
+
+// function carousel() {
+//   currentImageIndex++;
+//   if (currentImageIndex === imageArr.length) {
+//     currentImageIndex = 0;
+//     // if currentImageIndex is equal to the length of the imgArr,
+//     // set the currentImageIndex value to 0
+//   }
+//   changeDotColor();
+//   console.log(currentImageIndex);
+//   // change image below:
+//   $image.getAttribute('src', imageArr[currentImageIndex]);
+
+// }
+
+// var interval = setInterval(carousel, 2000);
+// // carousel();
